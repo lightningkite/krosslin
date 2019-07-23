85c1114ac244de9a6a28edfd060843f7ec775bea
Joseph Ivie <josephivie@gmail.com>
2019-07-23 11:44:42 -0600

    iOS improvements

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
         mavenLocal()
         maven("https://dl.bintray.com/lightningkite/com.lightningkite.krosslin")
         maven("https://dl.bintray.com/kotlin/kotlin-eap")
+        maven("https://dl.bintray.com/kotlin/kotlin-dev")
     }
     resolutionStrategy {
         eachPlugin {
